2987e3d28126a28e99703ce2d617d2915955df73
numbersevenb2 <gonzalezpuentejuanalberto3@gmail.com>
2024-06-03 12:46:44 -0600

    Dialogos 1

---
+++ Dialogos/Dialogos_Enemigos/Dialogo_Jabali.h
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+class DialogosJabaliEscenario1{
+    public:
+        //Primera parte de la conversacion con buen carma
+        void jabaliDialogoBueno_A1(){
+            cout<<"Hola soy Poppa, un certidooooo!!!?"<<endl;
+            cout<<"Perdon, aveces me exalto un poco."<<endl;
+        }
+        void jabaliDialogoBueno_A2(){
+            cout<<"No seeeee!!!??, disculpa, aveces me exalto, nos encontramos"<<endl;
+            cout<<"en la entrada de Avismord el calavozo olvidado por internet donde los desechos vienen a parar."<<endl;
+        }
+        void jabaliDialogoBueno_A3(){
+            cout<<"No puedo, estoy ocupadooo!!"<<endl;
+            cout<<"Ocupate de tus asuntos, perdon"<<endl;
+        }
+        void jabaliDialogoBueno_A4(){
+            cout<<"Adios rarito."<<endl;
+        }
+
+        //Segunda parte de la conversacion con buen karma
+        void jabaliDialogoBueno_B1(){
+            cout<<"Bueno, seguire en lo mio adios!!!"<<endl;
+        }
+        void jabaliDialogoBueno_B2(){
+            cout<<"Eres raro..."<<endl;
+        }
+        void jabaliDialogoBueno_B3(){
+            cout<<"De nadaAAAA!!"<<endl;
+        }
+        void jabaliDialogoBueno_B4(){
+            cout<<"......"<<endl;
+        }
+
+        //Primera parte de la conversacion con mal karma
+        void jabaliDialogoMalo_A1(){
+            cout<<"Ahhh que te importaaA!!?"<<endl;
+        }
+        void jabaliDialogoMalo_A2(){
+            cout<<"No molestas, preguntale a alguien mazzzz!"<<endl;
+        }
+        void jabaliDialogoMalo_A3(){
+            cout<<"No estoy de humooooor, mejor vamos a pelear!!"<<endl;
+        }
+        void jabaliDialogoMalo_A4(){
+            cout<<"Adonde crees que vazzz!!!"<<endl;
+            cout<<"Pelea cosa rara!!"<<endl;
+        }
+
+        //Segunda parte de la conversacion con buen karma
+        void jabaliDialogoMalo_B1(){
+            cout<<"!?!?"<<endl;
+        }
+        void jabaliDialogoMalo_B2(){
+            cout<<">:("<<endl;
+        }
+        void jabaliDialogoMalo_B3(){
+            cout<<"!!!!"<<endl;
+        }
+        void jabaliDialogoMalo_B4(){
+            cout<<"...."<<endl;
+        }
+};
